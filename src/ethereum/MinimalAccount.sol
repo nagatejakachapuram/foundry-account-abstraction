cc265a2836bf7e0fd960f04a62dd76e1c6de19a2
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-02-23 21:31:57 +0530

    Update MinimalAccount.sol
    Added a basic code for creating AA on any EVM Compatible chain.

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    
+    constructor() Ownable(msg.sender) {}
+    // A signature is valid if it's the Minimal account owner
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        _payPreFund(missingAccountFunds);
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPreFund(uint256 missingAccountFunds) internal {
+        if(missingAccountFunds !=0){
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}(" ");
+            (success);
+        }
+
+    }
+}
